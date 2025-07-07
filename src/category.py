6dc6c37e3cac95996044c83ec855643f9ba54915
shn73 <shn-73@mail.ru>
2025-07-07 12:21:44 +0300

    feature/homework_14_1 change_3

--- src/category.py
+++ src/category.py
@@ -1,3 +1,4 @@
+from src.product import  Product
 
 
 class Category:
@@ -10,6 +11,23 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.category_count += 1
         Category.product_count += len(products) if products else 0
+
+
+    def add_product(self, product: Product):
+        self.__products.append(product)
+        Category.product_count += product.quantity
+
+    @property
+    def products(self):
+        products_str = ""
+        for product in self.__products:
+            products_str += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return products_str
+
+    @products.setter
+    def products(self, product: Product):
+        self.__products.append(product)
+        Category.product_count += 1
--- src/product.py
+++ src/product.py
@@ -8,5 +8,26 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+
+
+    @classmethod
+    def new_product(cls, product_data):
+        name = product_data["name"]
+        description = product_data["description"]
+        price = float(product_data["price"])
+        quantity = int(product_data["quantity"])
+        return cls(name, description, price, quantity)
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price: str):
+        if int(new_price) <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+            return
+        self.__price = new_price
