1b196095e6cd497cef3725370cae4cbc103c0860
shn73 <shn-73@mail.ru>
2025-07-07 19:20:08 +0300

    feature/homework_14_1 change_5

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,3 +1,5 @@
+from src.product import Product
+
 
 def test_product_init(first_product, second_product):
     assert first_product.name == 'Samsung Galaxy S23 Ultra'
@@ -9,3 +11,13 @@ def test_product_init(first_product, second_product):
     assert second_product.description == '512GB, Gray space'
     assert second_product.price == 210000.0
     assert second_product.quantity == 8
+
+
+def test_new_product():
+    new_product = Product.new_product(
+        {"name": "Samsung Galaxy S23 Ultra", "description": "256GB, Серый цвет, 200MP камера", "price": 180000.0,
+         "quantity": 5})
+    new_product.name = "Samsung Galaxy S23 Ultra"
+    new_product.description = "256GB, Серый цвет, 200MP камера"
+    new_product.price = 180000.0
+    new_product.quantity = 5
