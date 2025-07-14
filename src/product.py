00d92a5d131b6e9334fcefaec7a49e52f010c9cf
shn73 <shn-73@mail.ru>
2025-07-14 18:23:22 +0300

    feature/homework_16_2 change1

--- src/product.py
+++ src/product.py
@@ -1,5 +1,8 @@
+from src.base_product import BaseProduct
+from src.print_mixin import PrintMixin
 
-class Product:
+
+class Product(BaseProduct, PrintMixin):
     name: str
     description: str
     price: float
@@ -10,6 +13,7 @@ class Product:
         self.description = description
         self.__price = price
         self.quantity = quantity
+        super().__init__()
 
     def __str__(self):
         return f"{self.name}, {self.__price} руб. Остаток: {self.quantity} шт."
