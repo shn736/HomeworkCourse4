f956f73ea21c19e887fb314b06d14a9f82d44a86
shn73 <shn-73@mail.ru>
2025-07-15 17:45:06 +0300

    feature/homework_17_1 change 1

--- src/category.py
+++ src/category.py
@@ -1,3 +1,5 @@
+from itertools import product
+
 from src.product import Product
 
 
@@ -43,3 +45,9 @@ class Category:
     @property
     def category_in_products(self):
         return self.__products
+
+    def middle_price(self):
+        try:
+            return sum([product.price for product in self.__products]) / len(self.__products)
+        except ZeroDivisionError:
+            return 0
